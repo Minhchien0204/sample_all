d990409598dc1cf665ec7cfb80a763edcdbed8d2
Le Phu Cuong <dongthoigian31313@gmail.com>
2019-03-24 20:14:27 +0700

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,10 +11,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
-   test "valid signup information" do
+  test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post users_path, params: { user: { name:  "Example User",
@@ -24,6 +22,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.FILL_IN
+    assert_not flash[success: "Success", error: "Error"]
+    assert is_logged_in?
   end
 end
